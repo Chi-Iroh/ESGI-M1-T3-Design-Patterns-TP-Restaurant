9fe1f0afb0fc456fac9377511d30361a078ef2c2
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2026-07-20 16:15:11 +0200

    Add work hours

--- RestaurantData.cs
+++ RestaurantData.cs
@@ -26,5 +26,20 @@ namespace RestaurantApi
                 ])
             ];
         }
+
+        public Dictionary<string, string> WorkHours()
+        {
+            Dictionary<string, string> workHours = new();
+
+            workHours.Add("Monday", "12PM - 7PM");
+            workHours.Add("Tuesday", "12PM - 7PM");
+            workHours.Add("Wednesday", "12PM - 7PM");
+            workHours.Add("Thursday", "12PM - 7PM");
+            workHours.Add("Friday", "12PM - 7PM");
+            workHours.Add("Saturday", "12PM - 2PM");
+            workHours.Add("Sunday", "Closed");
+
+            return workHours;
+        }
     }
 }
